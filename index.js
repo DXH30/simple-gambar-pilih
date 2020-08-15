e8b9c18ac0d3bdf6fa66f87b0390ed28ba760e96
DXH30 <dhs0223@gmail.com>
2020-08-16 02:18:43 +0800

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-// Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const angka1 = document.getElementById("angka1").value;
+const angka2 = document.getElementById("angka2").value;
+const operator = document.getElementById("operator").value;
+
+function Hitung() {
+  console.log(angka1);
+  console.log(angka2);
+  console.log(operator);
+}
